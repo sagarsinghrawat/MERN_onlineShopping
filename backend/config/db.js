f639896d9dfdf189a05b0ad01e087789fb416c71
sagarsinghrawat <sagarrawatssr9647@gmail.com>
2020-12-14 20:16:36 +0530

    add database connection file

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect( process.env.MONGO_URI,{
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+        
+        console.log(`MongoDB Connected: ${connect.connection.host}`)
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
