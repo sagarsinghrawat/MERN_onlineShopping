299c8fc9f48be21aa78fd88fa7f441acb900c7cf
sagarsinghrawat <sagarrawatssr9647@gmail.com>
2020-12-17 02:17:13 +0530

    admin services add\

---
+++ frontend/src/services/adminService.js
@@ -0,0 +1,203 @@
+import axios from 'axios'
+import { 
+    ADMIN_DELETE_FAIL, 
+    ADMIN_DELETE_REQUEST, 
+    ADMIN_DELETE_SUCCESS, 
+    GET_ORDERS_BY_ADMIN_FAIL, 
+    GET_ORDERS_BY_ADMIN_REQUEST, 
+    GET_ORDERS_BY_ADMIN_SUCCESS, 
+    PRODUCT_ADD_BY_ADMIN_FAIL, 
+    PRODUCT_ADD_BY_ADMIN_REQUEST, 
+    PRODUCT_ADD_BY_ADMIN_SUCCESS, 
+    PRODUCT_DELETE_BY_ADMIN_FAIL, 
+    PRODUCT_DELETE_BY_ADMIN_REQUEST, 
+    PRODUCT_DELETE_BY_ADMIN_SUCCESS, 
+    PRODUCT_UPDATE_BY_ADMIN_FAIL, 
+    PRODUCT_UPDATE_BY_ADMIN_REQUEST, 
+    PRODUCT_UPDATE_BY_ADMIN_SUCCESS, 
+    USER_UPDATE_BY_ADMIN_FAIL, 
+    USER_UPDATE_BY_ADMIN_REQUEST, 
+    USER_UPDATE_BY_ADMIN_SUCCESS 
+    } from "../utils/adminConstants";
+import { USER_DETAILS_SUCCESS } from '../utils/userConstants';
+
+export const userDeleteByAdmin = (id) => async( dispatch, getState ) => {
+
+    try {
+
+        dispatch({ type: ADMIN_DELETE_REQUEST })
+
+        const { userLogin : { userInfo } } = getState();
+
+        const config = {
+            headers : { 
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        await axios.delete(`/api/users/${id}`, config )
+
+        dispatch({
+            type: ADMIN_DELETE_SUCCESS,
+        })
+
+    } catch (error) {
+
+        dispatch({
+            type: ADMIN_DELETE_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
+
+export const userUpdateByAdmin = (user) => async( dispatch, getState ) => {
+
+    try {
+
+        dispatch({ type: USER_UPDATE_BY_ADMIN_REQUEST })
+
+        const { userLogin : { userInfo } } = getState();
+
+        const config = {
+            headers : { 
+                "Content-type": "application/json",
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.put(`/api/users/${user._id}`, user, config )
+
+        dispatch({ type: USER_UPDATE_BY_ADMIN_SUCCESS })
+
+        dispatch({
+            type: USER_DETAILS_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+
+        dispatch({
+            type: USER_UPDATE_BY_ADMIN_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
+
+export const deleteProductByAdmin = (id) => async( dispatch, getState ) => {
+
+    try {
+
+        dispatch({ type: PRODUCT_DELETE_BY_ADMIN_REQUEST })
+
+        const { userLogin : { userInfo } } = getState();
+
+        const config = {
+            headers : { 
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        await axios.delete(`/api/products/${id}`, config )
+
+        dispatch({ type: PRODUCT_DELETE_BY_ADMIN_SUCCESS })
+
+    } catch (error) {
+
+        dispatch({
+            type: PRODUCT_DELETE_BY_ADMIN_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
+
+export const productAddByAdmin = () => async( dispatch, getState ) => {
+
+    try {
+
+        dispatch({ type: PRODUCT_ADD_BY_ADMIN_REQUEST })
+
+        const { userLogin : { userInfo } } = getState();
+
+        const config = {
+            headers : { 
+                //"Content-type": "application/json",
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.post(`/api/products`, {}, config )
+
+        dispatch({ 
+            type: PRODUCT_ADD_BY_ADMIN_SUCCESS,
+            payload: data
+         })
+
+    } catch (error) {
+
+        dispatch({
+            type: PRODUCT_ADD_BY_ADMIN_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
+
+export const updateProductByAdmin = (product) => async( dispatch, getState ) => {
+
+    try {
+
+        dispatch({ type: PRODUCT_UPDATE_BY_ADMIN_REQUEST })
+
+        const { userLogin : { userInfo } } = getState();
+
+        const config = {
+            headers : { 
+                "Content-type": "application/json",
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.put(`/api/products/${product._id}`, product, config )
+
+        dispatch({ 
+            type: PRODUCT_UPDATE_BY_ADMIN_SUCCESS,
+            payload: data
+         })
+
+    } catch (error) {
+
+        dispatch({
+            type: PRODUCT_UPDATE_BY_ADMIN_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
+
+export const fetchOrdersByAdmin = () => async( dispatch, getState ) => {
+
+    try {
+
+        dispatch({ type: GET_ORDERS_BY_ADMIN_REQUEST })
+
+        const { userLogin : { userInfo } } = getState();
+
+        const config = {
+            headers : { 
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.get(`/api/orders/`, config )
+
+        dispatch({ 
+            type: GET_ORDERS_BY_ADMIN_SUCCESS,
+            payload: data
+         })
+
+    } catch (error) {
+
+        dispatch({
+            type: GET_ORDERS_BY_ADMIN_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
