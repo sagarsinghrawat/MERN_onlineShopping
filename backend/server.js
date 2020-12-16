32e38a9f099248673fef759df38ca346f547d0b0
sagarsinghrawat <sagarrawatssr9647@gmail.com>
2020-12-17 00:50:38 +0530

    add server file

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path')
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const productRoutes = require('./routes/productRoutes')
+const userRoutes = require('./routes/userRoutes')
+const orderRoutes = require('./routes/orderRoutes')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
+const imageUploadRoutes = require('./routes/imageUploadRoutes');
+
+
+const app = express();
+app.use(express.json());
+
+dotenv.config();
+
+connectDB();
+
+app.get('/', (req, res) => {
+    res.send('Api Running..')
+});
+
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/uploads', imageUploadRoutes);
+
+app.get('/api/config/paypal', ( req, res) => {
+    res.send(process.env.PAYPAL_CLIENT_ID)
+})
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')) );
+
+app.use(notFound)
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen( PORT, console.log(`Listining to ${process.env.NODE_ENV} server Port ${PORT}`));
