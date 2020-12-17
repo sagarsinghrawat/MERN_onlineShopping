9ff0379aef43f77e041d1bbfb35bf0b56ea4cabd
sagarsinghrawat <sagarrawatssr9647@gmail.com>
2020-12-17 22:14:34 +0530

    fix some bug

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,8 @@ app.get('/api/config/paypal', ( req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID)
 })
 
+const __dirname = path.resolve();
+
 if( process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')));
 
