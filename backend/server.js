a11ae04617fdfce00d29c7c192d4b3e1a091561d
sagarsinghrawat <sagarrawatssr9647@gmail.com>
2020-12-17 22:02:34 +0530

    fix some bug

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ if( process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')));
 
     app.get('*', (req, res) => {
-        res.send(path.resolve(__dirname, 'frontend', 'build', 'index.html' ))
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html' ))
     })
 } else {
     app.get('/', (req, res) => {
