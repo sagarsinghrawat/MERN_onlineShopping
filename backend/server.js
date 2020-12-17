118d9d652864510a1585491ee10653c60b5e33f4
sagarsinghrawat <sagarrawatssr9647@gmail.com>
2020-12-17 22:39:39 +0530

    fix some bug

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,9 @@ dotenv.config();
 
 connectDB();
 
-if( process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
-}
+// if( process.env.NODE_ENV === 'development'){
+//     app.use(morgan('dev'));
+// }
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -30,19 +30,18 @@ app.get('/api/config/paypal', ( req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID)
 })
 
-const __dirname = path.resolve();
 
-if( process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
+// if( process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html' ))
-    })
-} else {
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html' ))
+//     })
+// } else {
     app.get('/', (req, res) => {
         res.send('Api Running..')
     });
-}
+// }
 
 app.use('/images', express.static('/images') );
 
