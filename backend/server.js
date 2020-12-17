24cc2aa2ceaa1415c0dec61a08e3f56374f343f2
sagarsinghrawat <sagarrawatssr9647@gmail.com>
2020-12-17 18:20:24 +0530

    fixed some routes and path

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.get('/api/config/paypal', ( req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID)
 })
 
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')) );
+app.use('/images', express.static('/images') );
 
 app.use(notFound)
 app.use(errorHandler);
